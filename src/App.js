862aa7143adf1b1dd33ab9094ddbdd715247747b
ejsjose <ejsjose@kyndryl.com>
2022-05-12 07:56:46 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div>
+        <h1>meu primeiro texto</h1>
+        <Nome/>
+    </div>
+  );
+}
+
+export default App;
+
+function Nome(){
+    return (
+        <span>Bem-vindo : </span>
+    );
+}
